b9e00324f03ba0d4da6e95fdc2ae048756f8da8a
kirans0325 <kirans0325@gmail.com>
2025-10-03 20:33:51 +0530

    part 1 courseinfo exercise 1.2

--- part-1/courseinfo/src/App.jsx
+++ part-1/courseinfo/src/App.jsx
@@ -13,27 +13,26 @@ const App = () => {
     </div>
   }
 
-  const Content=({part1,part2,part3,exercises1,exercises2,exercises3})=>{
-    return <div>
-      <p>{part1}{exercises1} </p>
-      <p>{part2}{exercises2} </p>
-      <p>{part3}{exercises3} </p>
-    </div>
-  }
-
   const Total =({exercises1,exercises2,exercises3})=>{
     return <div>
       <p>Number of exercises : {exercises1+exercises2+exercises3} </p>
+    </div>
+  }
+  const Part=({part,exercises})=>{
+    return <div>
+        <p>{part}{exercises}</p>
     </div>
   }
 
   return (
     <div>
       <Header course={course}/>
-      <Content part1={part1} exercises1={exercises1} 
-      part2={part2} exercises2={exercises2}
-      part3={part3} exercises3={exercises3}
-      />
+
+
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
+
      <Total  exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
        
     </div>

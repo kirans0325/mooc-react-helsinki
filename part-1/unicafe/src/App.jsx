a3694a29958269a0b9e14f3eacf87fccc452d6ab
kirans0325 <kirans0325@gmail.com>
2025-10-05 12:26:59 +0530

    part 1 unicafe  exercise 1.11

--- part-1/unicafe/src/App.jsx
+++ part-1/unicafe/src/App.jsx
@@ -9,35 +9,40 @@ const Button = ({ onClick, text }) => {
 
 const StatisticLine = ({ text, value }) => {
   return (
-    <div>
-      <p>
-        {text}:{value}
-      </p>
-    </div>
+  
+        <tr>
+          <td> {text} </td>
+          <td>  {value} </td>
+        </tr>
+
   );
 };
 const Stastics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad;
   const average = (good * 1 + neutral * 0 + bad * -1) / total;
   const positive = (good / (total || 1)) * 100;
-  if (good === 0 && neutral === 0 && bad === 0) {
+  if (total === 0) {
     return <p>No feed back given</p>;
   } else {
     return (
       <div>
-        <StatisticLine text={"good"} value={good} />
-        <StatisticLine text={"neutral"} value={neutral} />
-        <StatisticLine text={"bad"} value={bad} />
-        <StatisticLine text={"all"} value={total} />
-        <StatisticLine text={"average"} value={average} />
-        <StatisticLine text={"positive"} value={positive} />
+         <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={total} />
+        <StatisticLine text="average" value={average.toFixed(2)} />
+        <StatisticLine text="positive" value={positive.toFixed(2) + " %"} />
+      </tbody>
+    </table>
       </div>
     );
   }
 };
 
 const App = () => {
-  // save clicks of each button to its own state
+  // save clicks           of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
@@ -62,7 +67,6 @@ const App = () => {
       <Button onClick={handleNeutral} text={"neutral"} />
       <Button onClick={handleBad} text={"bad"} />
       <Header heading={"Stastics"} />
-
       <Stastics good={good} neutral={neutral} bad={bad} />
     </div>
   );

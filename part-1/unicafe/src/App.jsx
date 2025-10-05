e2553f7ab47c08a729f8d474bfbdbd7808d0d9bc
kirans0325 <kirans0325@gmail.com>
2025-10-05 10:28:07 +0530

    part 1 unicafe  exercise 1.7

--- part-1/unicafe/src/App.jsx
+++ part-1/unicafe/src/App.jsx
@@ -1,37 +1,56 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-const Header =(props)=>{
-  return <h1>{props.heading}</h1>
-}
-const Button=({onClick,text})=>{
-  return <button onClick={onClick}>{text}</button>
-}
+const Header = (props) => {
+  return <h1>{props.heading}</h1>;
+};
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
 
-const Total =({good, neutral, bad})=>{
-  return <div>
-    <p>good : {good}</p>
-    <p>neutral: {neutral}</p>
-    <p>bad : {bad}</p>
-  </div>
-}
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
- 
+const Total = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = (good * 1 + neutral * 0 + bad * -1) / total;
+  const positive = (good / (total || 1)) * 100;
   return (
     <div>
-      <Header heading={"give feedback"}/>
-      <Button onClick={()=>setGood(good+1)} text={"good"}/>
-      <Button onClick={()=>setNeutral(good+1)}text={"neutral"}/>
-      <Button onClick={()=>setBad(good+1)}text={"bad"}/>
-      <Header heading={"Stastics"} />
-      {console.log(good)}
-      <Total good={good} neutral={neutral} bad={bad}/>
+      <p>good : {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad : {bad}</p>
+      <p>all :{good + neutral + bad}</p>
+      <p>average:{average} </p>
+      <p>positive:{positive}% </p>
     </div>
-  )
-}
+  );
+};
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-export default App
+  const handleBad = () => {
+    const badClick = bad + 1;
+    setBad(badClick);
+  };
+  const handleNeutral = () => {
+    const neutralClick = neutral + 1;
+    setNeutral(neutralClick);
+  };
+  const handleGood = () => {
+    const goodClick = good + 1;
+    setGood(goodClick);
+  };
+
+  return (
+    <div>
+      <Header heading={"give feedback"} />
+      <Button onClick={handleGood} text={"good"} />
+      <Button onClick={handleNeutral} text={"neutral"} />
+      <Button onClick={handleBad} text={"bad"} />
+      <Header heading={"Stastics"} />
+      <Total good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+};
+
+export default App;

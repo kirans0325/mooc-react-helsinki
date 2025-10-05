20a08b53fad3dd51289e051365ccfa89a6cee5fd
kirans0325 <kirans0325@gmail.com>
2025-10-05 14:20:35 +0530

    part 1 anecdote exercise 1.14 & completion of part-1

--- part-1/anecdote/src/App.jsx
+++ part-1/anecdote/src/App.jsx
@@ -1,44 +1,55 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
   const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
-   
-  const [selected, setSelected] = useState(0)
-  const [vote, setVote] =useState(Array(anecdotes.length).fill(0))
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
 
-  const handleVote=()=>{
-    const newVote= [...vote]
-    newVote[selected]+=1
-    setVote(newVote)
+  const [selected, setSelected] = useState(0);
+  const [vote, setVote] = useState(Array(anecdotes.length).fill(0));
 
-  }
-    
+  const handleVote = () => {
+    const newVote = [...vote];
+    newVote[selected] += 1;
+    setVote(newVote);
+  };
+
   const handleNext = () => {
     let randomIndex = 0;
     do {
-      randomIndex = Math.floor(Math.random() * anecdotes.length)
-    } while (randomIndex === selected)
- 
-    setSelected(randomIndex)
-  }
-  
+      randomIndex = Math.floor(Math.random() * anecdotes.length);
+    } while (randomIndex === selected);
+
+    setSelected(randomIndex);
+  };
+  const maxVotes = Math.max(...vote);
+  const topAnectdoteIdx = vote.indexOf(maxVotes);
+
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       {anecdotes[selected]}
-       <p> has {vote[selected]} votes </p>
+      <p> has {vote[selected]} votes </p>
       <button onClick={handleVote}>Vote</button>
       <button onClick={handleNext}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      {maxVotes > 0 ? (
+        <>
+          <p> {anecdotes[topAnectdoteIdx]} </p>
+          <p> has {maxVotes} votes</p>
+        </>
+      ) : (
+        <p>No votes given </p>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;

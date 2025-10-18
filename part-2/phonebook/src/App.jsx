082d502d4814e4c929d14423aa435ed6ce92c646
kirans0325 <kirans0325@gmail.com>
2025-10-19 00:06:59 +0530

    part 2b phonebook 2.15

--- part-2/phonebook/src/App.jsx
+++ part-2/phonebook/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
-import personService from "./services/notes"; // handles axios CRUD
+import personService from "./services/notes"; 
 
 const App = () => {
   const [persons, setPersons] = useState([]);
@@ -9,49 +9,68 @@ const App = () => {
   const [newNumber, setNewNumber] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
 
-  // Fetch all persons
+ 
   useEffect(() => {
     personService.getAll().then((response) => setPersons(response));
   }, []);
 
-  // Controlled input handlers
   const handleNameChange = (event) => setNewName(event.target.value);
   const handleNumberChange = (event) => setNewNumber(event.target.value);
   const handleSearchChange = (event) => setSearchTerm(event.target.value);
 
-  // Add a new person
   const addPerson = (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const nameExists = persons.some(
-      (person) => person.name.toLowerCase() === newName.toLowerCase()
+  const existingPerson = persons.find(
+    (person) => person.name.toLowerCase() === newName.toLowerCase()
+  );
+
+  const nameObject = {
+    name: newName,
+    number: newNumber,
+  };
+
+  if (existingPerson) {
+    const confirmUpdate = window.confirm(
+      `${newName} is already added to phonebook, replace the old number with a new one?`
     );
 
-    if (nameExists) {
-      alert(`${newName} is already added to phonebook`);
-      return;
+    if (confirmUpdate) {
+      
+      personService
+        .update(existingPerson.id, nameObject)
+        .then((returnedPerson) => {
+          setPersons(
+            persons.map((p) =>
+              p.id !== existingPerson.id ? p : returnedPerson
+            )
+          );
+          setNewName("");
+          setNewNumber("");
+        })
+        .catch((error) => {
+          alert(
+            `Information of ${existingPerson.name} has already been removed from server`
+          );
+          setPersons(persons.filter((p) => p.id !== existingPerson.id));
+        });
     }
-
-    const nameObject = {
-      name: newName,
-      number: newNumber,
-    };
-
+  } else {
+    
     personService.create(nameObject).then((createdPerson) => {
       setPersons(persons.concat(createdPerson));
       setNewName("");
       setNewNumber("");
     });
-  };
-
-  // Delete person with confirmation
+  }
+}; 
   const handleRemove = (id, name) => {
     const confirmDelete = window.confirm(`Delete ${name}?`);
     if (confirmDelete) {
       personService
         .remove(id)
         .then(() => {
-          // Update local state
+        
           setPersons(persons.filter((p) => p.id !== id));
         })
         .catch((error) => {
@@ -61,7 +80,7 @@ const App = () => {
     }
   };
 
-  // Filter results
+
   const personsToShow = persons.filter((person) =>
     person.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
@@ -83,9 +102,9 @@ const App = () => {
       <h3>Numbers</h3>
       <ul>
         {personsToShow.map((person) => (
-          <li key={person.number}>
+          <li key={person.id}>
             {person.name} {person.number}
-            <button onClick={() => handleRemove(person.id, person.name)}>
+            <button  onClick={() => handleRemove(person.id, person.name)}>
               delete
             </button>
           </li>
--- part-2/phonebook/src/services/notes.js
+++ part-2/phonebook/src/services/notes.js
@@ -1,18 +1,20 @@
-import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+import axios from "axios";
+const baseUrl = "http://localhost:3001/persons"; // adjust to your backend port
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
-const create = newObject => {
-  const request= axios.post(baseUrl, newObject)
-  return request.then(response=>response.data)
-}
-
-const remove = (id) => {
-  const request = axios.delete(`${baseUrl}/${id}`);
-  return request.then((response) => response.data);
+  return axios.get(baseUrl).then(response => response.data);
 };
 
-export default {  getAll, create,remove }
+const create = (newObject) => {
+  return axios.post(baseUrl, newObject).then(response => response.data);
+};
+
+const update = (id, newObject) => {
+  return axios.put(`${baseUrl}/${id}`, newObject).then(response => response.data);
+};
+
+const remove = (id) => {
+  return axios.delete(`${baseUrl}/${id}`).then(response => response.data);
+};
+
+export default { getAll, create, update, remove };

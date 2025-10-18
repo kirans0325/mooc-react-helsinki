e2a858934e965e5231414aac36e033d1794244bf
kirans0325 <kirans0325@gmail.com>
2025-10-18 10:40:12 +0530

    part 2b phonebook 2.11

--- part-2/phonebook/src/App.jsx
+++ part-2/phonebook/src/App.jsx
@@ -1,16 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 
 const App = () => {
+   const [notes, setNotes] = useState([])
   const [persons, setPersons] = useState([
     { name: "Arto Hellas", number: "040-123456", id: 1 },
     { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
     { name: "Dan Abramov", number: "12-43-234345", id: 3 },
     { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
-
+useEffect(() => {
+    console.log('effect')
+    const eventHanlder=(response)=>{
+      console.log("Promise fulfilled")
+      setNotes(response.data)
+    }
+    const promise= axios.get("http://localhost:3001/persons")
+    promise.then(eventHanlder)
+  }, [])
+  console.log('render', notes.length, 'notes')
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchTerm, setSearchTerm] = useState("");

60070ed74a3c67e595307f3cbea753d016a9caea
kirans0325 <kirans0325@gmail.com>
2025-10-22 06:13:40 +0530

    phone book backend 3.4 exercise

--- part-3/backend/index.js
+++ part-3/backend/index.js
@@ -1,71 +1,62 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.use(express.json());
 
 let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    },
-    { 
-      "id": "5",
-      "name": "Ram k", 
-      "number": "40-23-6423122"
-    }
-]
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+  {
+    id: "5",
+    name: "Ram k",
+    number: "40-23-6423122",
+  },
+];
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World</h1>');
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World</h1>");
 });
 
-app.get('/api/persons',(req,res)=>{
-  res.json(persons)
-})
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
 
-app.get('/info',(req,res)=>{
-  const noOfPersons=persons.length-2
-   const time = new Date()
+app.get("/info", (req, res) => {
+  const noOfPersons = persons.length - 2;
+  const time = new Date();
   res.send(`<p>phonebook has info of ${noOfPersons} people </p>
    <p> ${time}
-    <p>`)
-})
+    <p>`);
+});
 
-app.get('/api/persons/:id',(req,res)=>{
-  const id = req.params.id
-  const person=persons.find(p=>p.id==id)
-  person?res.json(person):res.status(404).end()
-
-})
-
-// app.get('/api/persons', (request, response) => {
-//   const id = request.params.id;
-//   const note = notes.find(note => note.id === id);
-//   note ? response.json(note) : response.status(404).end();
-//   console.log(request.headers)
-// });
-
-// app.delete('/api/notes/:id', (request, response) => {
-//   const id = request.params.id;
-//   notes = notes.filter(note => note.id !== id);
-//   response.status(204).end();
-// });
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const person = persons.find((p) => p.id === id);
+  person ? res.json(person) : res.status(404).end();
+});
+app.delete("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const person = persons.find((p) => p.id !== id);
+  res.status(204).end();
+});
 
 // const generateId = () => {
 //   const maxId = notes.length > 0
@@ -78,8 +69,8 @@ app.get('/api/persons/:id',(req,res)=>{
 //   const body = request.body
 
 //   if (!body.content) {
-//     return response.status(400).json({ 
-//       error: 'content missing' 
+//     return response.status(400).json({
+//       error: 'content missing'
 //     })
 //   }
 

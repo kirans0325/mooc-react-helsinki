0d695f6e24e89024f8c347908b7ce34253c9c979
kirans0325 <kirans0325@gmail.com>
2025-10-22 06:41:19 +0530

    phone book backend 3.5 exercise

--- part-3/backend/index.js
+++ part-3/backend/index.js
@@ -40,7 +40,7 @@ app.get("/api/persons", (req, res) => {
 });
 
 app.get("/info", (req, res) => {
-  const noOfPersons = persons.length - 2;
+  const noOfPersons = persons.length;
   const time = new Date();
   res.send(`<p>phonebook has info of ${noOfPersons} people </p>
    <p> ${time}
@@ -58,32 +58,29 @@ app.delete("/api/persons/:id", (req, res) => {
   res.status(204).end();
 });
 
-// const generateId = () => {
-//   const maxId = notes.length > 0
-//     ? Math.max(...notes.map(n => Number(n.id)))
-//     : 0
-//   return String(maxId + 1)
-// }
+const generateId = () => {
+  let newId;
+  do {
+    newId = String(Math.floor(Math.random() * 10000));
+  } while (persons.find((p) => p.id === newId));
+  return newId;
+};
 
-// app.post('/api/notes', (request, response) => {
-//   const body = request.body
 
-//   if (!body.content) {
-//     return response.status(400).json({
-//       error: 'content missing'
-//     })
-//   }
+app.post('/api/persons', (request, response) => {
+  const body = request.body
 
-//   const note = {
-//     content: body.content,
-//     important: body.important || false,
-//     id: generateId(),
-//   }
+    const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number || false,
+  }
+ 
 
-//   notes = notes.concat(note)
-
-//   response.json(note)
-// })
+  persons = persons.concat(person)
+  console.log(person)
+  response.json(person)
+})
 
 const PORT = 3001;
 app.listen(PORT, () => {

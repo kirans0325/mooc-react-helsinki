2aac96936b3b0a58bc14195dd933aadb047d322e
kirans0325 <kirans0325@gmail.com>
2025-10-24 22:56:28 +0530

    phone book 3.7&8 exercise

--- part-3/phonebook-backend/index.js
+++ part-3/phonebook-backend/index.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const app = express();
+const morgan = require('morgan')
+
+app.use(morgan('tiny'))
+
+morgan.token('body', (req) => {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body)
+  }
+  return ''
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 const persons = [
   { id: 1, name: "Arto Hellas", number: "040-123456" },
